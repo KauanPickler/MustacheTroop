70c5420e2b974907a81db6cda083d687b939f6ce
Kauan <fuel264strange@gmail.com>
2025-06-01 15:17:58 -0300

    Onze

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Clients from "./pages/Clients";
 import Team from "./pages/Team";
@@ -6,13 +6,12 @@ import Sales from "./pages/Sales";
 
 export default function App() {
   return (
-    <Router basename="MustacheTroop">
+    <Router>
       <div className="pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/clientes" element={<Clients />} />
           <Route path="/time" element={<Team />} />
-          <Route path="/vendas" element={<Sales />} />
           <Route path="/vendas" element={<Sales />} />
         </Routes>
       </div>
